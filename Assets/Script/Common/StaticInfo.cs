8cdad8531b0b148c08edc7cd8b61ac23c5782e41
KienAudi <phamkien1998>
2022-06-16 23:01:18 +0700

    kien add linh tinh

---
+++ Assets/Script/Common/StaticInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticInfo : MonoBehaviour
+{
+    public static string[] mapName =
+{
+        "",
+        "1.Map Kien 1",
+        "2.Map Kien 2",
+        "3.Map Kien 3",
+        "4.Map Kien 4",
+        "5.Map Kien 5",
+        "6.Map Kien 6",
+        "7.Map Kien 7",
+        "8.Map Kien 8",
+        "9.Map Kien 9",
+        "10.Map Kien 10",
+    };
+
+
+}
---
+++ Assets/Script/UIMainMenuController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class UIMainMenuController : MonoBehaviour
+{
+
+    public RectTransform Maps;
+    public TextMeshProUGUI mapName;
+    public TextMeshProUGUI mapDescription;
+
+    public Button leftBtn;
+    public Button rightBtn;
+
+    private int currentMap = 1;
+
+    private bool isClick = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectMap(currentMap);
+
+        leftBtn.onClick.AddListener(() => leftButton());
+        rightBtn.onClick.AddListener(() => rightButton());
+
+    }
+
+    private void leftButton()
+    {
+        if (isClick)
+        {
+            isClick = false;
+
+            currentMap--;
+            if (currentMap == 0)
+            {
+                currentMap = 10;
+            }
+            selectMap(currentMap);
+        }
+    }
+
+    private void rightButton()
+    {
+        if (isClick)
+        {
+            isClick = false;
+
+            currentMap++;
+            if (currentMap == 11)
+            {
+                currentMap = 1;
+            }
+            selectMap(currentMap);
+
+        }
+    }
+
+    private void selectMap(int index)
+    {
+        Maps.GetComponent<Image>().sprite = Resources.Load<Sprite>("UI/Maps/map_"+ index);
+        mapName.text = StaticInfo.mapName[index];
+        //mapDescription.text = "/10";
+
+
+        StartCoroutine(animationMap());
+        StartCoroutine(animationText());
+
+    }
+
+    IEnumerator animationMap()
+    {
+        Maps.DOScale(new Vector3(1.2f, 1.2f, 1f), 0.07f);
+        yield return new WaitForSeconds(0.1f);
+        Maps.DOScale(new Vector3(1f, 1f, 1f), 0.07f);
+    }
+
+    IEnumerator animationText()
+    {
+        mapName.GetComponent<RectTransform>().DOScale(new Vector3(0f, 0f, 0f), 0.1f);
+        mapDescription.GetComponent<RectTransform>().DOScale(new Vector3(0f, 0f, 0f), 0.1f);
+        yield return new WaitForSeconds(0.1f);
+        mapName.GetComponent<RectTransform>().DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+        mapDescription.GetComponent<RectTransform>().DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+
+
+        yield return new WaitForSeconds(0.5f);
+        isClick = true;
+
+    }
+
+    
+}

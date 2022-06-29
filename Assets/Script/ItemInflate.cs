22eed657189a1a92428a55b47eca531940cafccb
rewwzy <honghai3501@gmail.com>
2022-06-29 23:40:34 +0700

    try push

---
+++ Assets/Script/ItemInflate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class ItemInflate : MonoBehaviour
+{
+
+    public Button button;
+    public Image rarity;
+    public Image item;
+    public TextMeshProUGUI info;
+    public GameObject focus;
+    public GameObject lockButton;
+    //private ItemInventory itemData;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    button.onClick.AddListener(() => onClickItem());
+    //}
+    //private void onClickItem()
+    //{
+    //    if (itemData != null)
+    //    {
+    //        InventoryController.Instance.onClickItem(itemData);
+    //        setFocus(true);
+    //    }
+    //}
+}

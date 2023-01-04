c5b7dca09e0d07b06b484ecb19b914b4fd7fcb54
KienAudi <phamkien1998>
2023-01-04 21:56:33 +0700

    update logic for ui shop

--- Assets/Script/Database/ItemDatabase.cs
+++ Assets/Script/Database/ItemDatabase.cs
@@ -198,6 +198,7 @@ public class ItemDatabase : Singleton<ItemDatabase>
     {
         return inventoryData;
     }
+
     // common = 1 , uncommon = 2, rare = 3, mythical = 4, arcana = 5
     public void addNewItem(int id, int slot)
     {
@@ -245,38 +246,39 @@ public class ItemDatabase : Singleton<ItemDatabase>
             item.Stats_1 = 0;
             item.Stats_2 = 0;
             item.Stats_3 = 0;
+            item.Stats_4 = 0;
             if (item.Rarity == 1)
             {
-                int randomValue = getStatsItem(item.Type);
-                int randomStat = Random.Range(1, 3);
+                int randomValue = Random.Range(1, 7);
+                int randomStat = Random.Range(1, 3); // dang sai
                 item.Stats_1 = randomValue * 100 + randomStat;
             } else if(item.Rarity == 2)
             {
-                int randomValue = getStatsItem(item.Type);
+                int randomValue = Random.Range(1, 7);
                 int randomStat = Random.Range(1, 4);
                 item.Stats_1 = randomValue * 100 + randomStat;
             }
             else if(item.Rarity == 3)
             {
-                int randomValue = getStatsItem(item.Type);
+                int randomValue = Random.Range(1, 7);
                 int randomStat = Random.Range(1, 4);
                 item.Stats_1 = randomValue * 100 + randomStat;
 
-                int randomValue1 = getStatsItem(item.Type);
+                int randomValue1 = Random.Range(1, 7);
                 int randomStat1 = Random.Range(1, 4);
                 item.Stats_2 = randomValue1 * 100 + randomStat1;
             }
             else
             {
-                int randomValue = getStatsItem(item.Type);
+                int randomValue = Random.Range(1, 7);
                 int randomStat = Random.Range(1, 4);
                 item.Stats_1 = randomValue * 100 + randomStat;
 
-                int randomValue1 = getStatsItem(item.Type);
+                int randomValue1 = Random.Range(1, 7);
                 int randomStat1 = Random.Range(1, 4);
                 item.Stats_2 = randomValue1 * 100 + randomStat1;
 
-                int randomValue2 = getStatsItem(item.Type);
+                int randomValue2 = Random.Range(1, 7);
                 int randomStat2 = Random.Range(1, 4);
                 item.Stats_3 = randomValue2 * 100 + randomStat2;
             }
@@ -285,6 +287,102 @@ public class ItemDatabase : Singleton<ItemDatabase>
         Save();
     }
 
+    public void addNewItem(int id, int slot, int rarity)
+    {
+        ItemData rawItem = fetchItemById(id);
+        // Add consume item
+        if (rawItem.Type == 0)
+        {
+            ItemInventory item = new ItemInventory();
+            item.Id = rawItem.Id;
+            item.Name = rawItem.Name;
+            item.Contents = rawItem.Contents;
+            item.Rarity = rawItem.Rarity;
+            item.Type = rawItem.Type;
+            item.ShopId = 0;
+            item.IsUse = 0;
+            item.Level = 0;
+            item.Stats_1 = 0;
+            item.Stats_2 = 0;
+            item.Stats_3 = 0;
+            int index = fetchInventoryByIndex(id);
+            if (index == -1)
+            {
+                item.Slot = slot;
+                inventoryData.Add(item);
+            }
+            else
+            {
+                inventoryData[index].Slot += slot;
+            }
+            Save();
+        }
+        else
+        {
+            ItemInventory item = new ItemInventory();
+            item.Id = rawItem.Id;
+            item.Name = rawItem.Name;
+            item.Contents = rawItem.Contents;
+            item.Rarity = rarity;
+            item.Type = rawItem.Type;
+
+            item.Slot = 1;
+            item.ShopId = Random.Range(0, 999999);
+            item.IsUse = 0;
+            item.Level = 1;
+            item.Stats_1 = 0;
+            item.Stats_2 = 0;
+            item.Stats_3 = 0;
+            item.Stats_4 = 0;
+
+            if (item.Rarity >= 1)
+            {
+                int randomValue = Random.Range(1, 7);           
+                int randomStat = returnRandomStats(randomValue); 
+                item.Stats_1 = randomValue * 100 + randomStat;
+            }
+            if (item.Rarity >= 2)
+            {
+                int randomValue = Random.Range(1, 7);
+                int randomStat = returnRandomStats(randomValue);
+                item.Stats_2 = randomValue * 100 + randomStat;
+            }
+            if (item.Rarity >= 3)
+            {
+                int randomValue = Random.Range(1, 7);
+                int randomStat = returnRandomStats(randomValue);
+                item.Stats_3 = randomValue * 100 + randomStat;
+            }
+            if (item.Rarity >= 4)
+            {
+                int randomValue = Random.Range(1, 7);
+                int randomStat = returnRandomStats(randomValue);
+                item.Stats_4 = randomValue * 100 + randomStat;
+            }
+            inventoryData.Add(item);
+        }
+        Save();
+    }
+
+    private int returnRandomStats(int stat)
+    {
+        switch (stat)
+        {
+            case 1:
+                return Random.Range(10, 21);
+            case 2: 
+                return Random.Range(20, 41);
+            case 3:
+                return Random.Range(5, 16);
+            case 4:
+                return Random.Range(20, 41);
+            case 5:
+                return Random.Range(20, 41);
+            case 6:
+                return Random.Range(20, 41);
+        }
+        return 0;
+    }
     private int getStatsItem(int type)
     {
         if(type == 1)
@@ -407,6 +505,7 @@ public class ItemInventory : ItemData
     public int Stats_1 { get; set; }
     public int Stats_2 { get; set; }
     public int Stats_3 { get; set; }
+    public int Stats_4 { get; set; }
     public int Level { get; set; }
 }
 public class ItemData

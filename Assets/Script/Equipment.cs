36de1c8ca4508d5feb0d20a70ac438e901fc2e58
KevinPlaystudios1992 <thinh18021992@gmail.com>
2023-03-02 15:44:51 +0700

    Equipment and Atlas

---
+++ Assets/Script/Equipment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Equipment : MonoBehaviour
+{
+    [SerializeField] GameObject item;
+    [SerializeField] Image container;
+    [SerializeField] Image image;
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] Sprite defaultImage;
+
+    public void InitData(ItemInventory item, Sprite sprite)
+    {
+        if (item.Type == 0) return;
+        levelText.text = "Lv." + item.Level.ToString();
+        image.sprite = sprite;
+    }   
+    
+    public void SwitchToDefaultSprite()
+    {
+        image.sprite = defaultImage;        
+    }
+}
